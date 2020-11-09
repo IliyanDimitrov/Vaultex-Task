a06e73dd8bdc1108fd7cdcc60b448516a2312a18
iliyand <iliqn1994@gmail.com>
2020-11-09 19:17:44 +0000

    Initial commit

---
+++ task/atmRefill.js
@@ -0,0 +1,55 @@
+/* 
+    Author: Iliyan Dimitrov
+    Date: 08/11/20
+
+    The program will need to verify if the ATM cash refill customer orders are valid based on criteria.
+
+    Each cassette can take up to 2000 notes and is always filled to the max.
+    Valid notes - £5, £10, £20, £50
+    Max 4 cassettes per order, if no cassette number is given default num = 4;
+
+    Format:
+            ['cassettes', 4],
+            ['5', 10000],
+            ['10', 10000],
+            ['20', 10000],
+            ['50', 10000]
+*/
+
+const validateOrder = order => {
+
+    let cassetteQty = 0;
+
+    //Check if we have cassette qty and assign it to a variable 
+    if (order[0][0] === 'cassettes') {
+        cassetteQty = order[0][1];
+        order.shift();
+    } else {
+        cassetteQty = 4;
+    }
+
+    //Check if all cassette orders are valid and fill up to capacity
+    let checkFillCapacity = order.map(el => {
+        return cassetteQty <= 4 && el[0] % 5 === 0 ?
+            el[1] / el[0] === 2000 ?
+                ['valid', 'Order valid, sent for packing'] :
+                ['error', `Insufficient amount of ${el[0]} notes`] :
+                ['error', 'Invalid Cassettes Number or Invalid bank note.']
+    });
+
+    //Gather all order validity messages in one container
+    const orderValidityMessages = [...new Set(checkFillCapacity)];
+
+    //In case of error return the error message
+        for (let el of orderValidityMessages) {
+            if (el[0] === 'error') {
+                return el;
+            }
+        }
+
+    return orderValidityMessages.flat();
+}
+
+module.exports = {
+    validateOrder
+};
---
+++ test/tests.test.js
@@ -0,0 +1,58 @@
+const { validateOrder } = require("../task/atmRefill");
+
+describe("validateOrder", () => {
+    test("returns an error if there is invalid number of bank notes or cassettes", () => {
+
+        const custOrder = [
+            ['cassettes', 5],
+            ['5', 10000],
+            ['10', 10000],
+            ['20', 10000],
+            ['50', 10000]
+        ];
+        expect(validateOrder(custOrder)).toEqual(['error', 'Invalid Cassettes Number or Invalid bank note.']);
+    });
+
+    test("returns an error if there is invalid number of bank notes or cassettes", () => {
+
+        const custOrder = [
+            ['cassettes', 4],
+            ['3', 10000],
+            ['10', 10000],
+            ['20', 10000],
+            ['50', 10000]
+        ];
+        expect(validateOrder(custOrder)).toEqual(
+            [
+                'error',
+                'Invalid Cassettes Number or Invalid bank note.']);
+    });
+
+    test("returns an error if there is invalid number of bank notes or cassettes", () => {
+
+        const custOrder = [
+            ['cassettes', 1],
+            ['3', 10000]
+        ];
+        expect(validateOrder(custOrder)).toEqual(['error', 'Invalid Cassettes Number or Invalid bank note.']);
+    });
+
+    test("returns true if there is sufficient number of bank notes to fulfill the request", () => {
+
+        const custOrder = [
+            ['cassettes', 1],
+            ['5', 10000]
+        ];
+        expect(validateOrder(custOrder)).toEqual(['valid', 'Order valid, sent for packing']);
+    });
+
+    test("returns true if there is sufficient number of bank notes to fulfill the request", () => {
+
+        const custOrder = [
+            ['cassettes', 2],
+            ['5', 10000],
+            ['2', 10000]
+        ];
+        expect(validateOrder(custOrder)).toEqual(['error', 'Invalid Cassettes Number or Invalid bank note.']);
+    });
+});
